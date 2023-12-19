9f306296855bd85f5c39a02f9ee5dc51c5e11354
RyanD177 <w0477770@campus.nscc.ca>
2023-12-18 20:26:34 -0400

    commit

--- my-react-quiz/src/quiz.jsx
+++ my-react-quiz/src/quiz.jsx
@@ -1,21 +1,58 @@
-import React, { useEffect } from 'react';
-
-const QUIZ_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/quiz';
+import React, { useState, useEffect } from "react";
 
 export default function Quiz() {
+  const [questions, setQuestions] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [chosenAnswers, setChosenAnswers] = useState([]);
 
-    useEffect(() => {
-        const [questions, setQuestions] = useState(null);
-            const callFetch = async () => {
-            const response = await fetch(QUIZ_API_BASE_URL);
-            const jsonResponse = await response.json();
-            
-        }
-    }, [])
+  useEffect(() => {
+    const callFetch = async () => {
+      const response = await fetch("/api/fe/quiz");
+      const jsonResponse = await response.json();
+      setQuestions(jsonResponse);
+    };
+    callFetch();
+  }, []);
+
+  if (questions == null) return null;
+
+    const updateChosenAnswers = (questionIndex, answerIndex) => {
+          const newChosenAnswers = [...chosenAnswers];
+          newChosenAnswers[questionIndex] = answerIndex;
+          setChosenAnswers(newChosenAnswers);  
+
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
 
   return (
     <>
-    
+      <h1>{currentQuestion.question}</h1>
+      {currentQuestion.answers.map((answer, answerIndex) => {
+            const chosenAnswer = chosenAnswers[currentQuestionIndex];
+            let className = 'answer'
+
+            if(chosenAnswer === answerIndex){
+                className += currentQuestion.correctAnswer === chosenAnswer ? ' correct' : ' incorrect';
+            }
+
+
+        return (
+          <h2
+            key={answer} // whenever map over an array to generate jsx we need to have unique keys
+            onClick={() => {
+                if(chosenAnswer != null) return;
+
+                updateChosenAnswers(currentQuestionIndex, answerIndex);
+
+            }}
+            className = {className}
+          >
+
+            {answer}
+          </h2>
+        );
+      })}
     </>
   );
 }
